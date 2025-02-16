ede8ec8389916848925732e68d490b46a4f2811f
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-16 20:24:55 +0600

    Create Loops.java

---
+++ Loops.java
@@ -0,0 +1,13 @@
+class Loops {
+  
+    public static void main(String args[]) {
+      
+        // for loop to print numbers from 1 to 5
+        for (int i = 1; i <= 5; i++) {
+          
+            System.out.println("" + i);
+        }
+        
+        System.out.println("Loop has ended.");
+    }
+              }
