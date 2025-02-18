505f112991882e653ed58732e7b6e34dc21308bb
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-18 09:16:53 +0600

    Create Method.java

---
+++ Method.java
@@ -0,0 +1,16 @@
+// Creating a method
+// that prints a message
+public class Method {
+  
+    // Method to print message
+    public void printMessage() {
+        System.out.println("Hello, Geeks!");
+    }
+
+    public static void main(String[] args) {
+      
+        // Create an instance of the Method class
+        Method m = new Method();
+        m.printMessage();  // Calling the method
+    }
+}
