45e948efa7705651dc51bbd18a54f01a5121f9e6
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-20 01:08:46 +0600

    Create Car.java

---
+++ Car.java
@@ -0,0 +1,47 @@
+// Define the Car class
+public class Car {
+    // Attributes of the Car class
+    private String make;
+    private String model;
+    private int year;
+
+    // Constructor to initialize the Car object
+    public Car(String make, String model, int year) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+    }
+
+    // Getter methods
+    public String getMake() {
+        return make;
+    }
+
+    public String getModel() {
+        return model;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    // Setter methods
+    public void setMake(String make) {
+        this.make = make;
+    }
+
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    // Method to display car details
+    public void displayDetails() {
+        System.out.println("Car Make: " + make);
+        System.out.println("Car Model: " + model);
+        System.out.println("Car Year: " + year);
+    }
+}
