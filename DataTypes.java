a1659247921fb2f54ce268c4838eb2500c94d0a3
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-21 09:59:52 +0600

    Create DataTypes.java

---
+++ DataTypes.java
@@ -0,0 +1,27 @@
+public class DataTypes {
+    public static void main(String[] args) {
+        // Integer data type
+        int myInt = 100;
+        System.out.println("Integer: " + myInt);
+
+        // Floating point data type
+        float myFloat = 10.5f;
+        System.out.println("Float: " + myFloat);
+
+        // Double data type
+        double myDouble = 20.99;
+        System.out.println("Double: " + myDouble);
+
+        // Character data type
+        char myChar = 'A';
+        System.out.println("Character: " + myChar);
+
+        // Boolean data type
+        boolean myBool = true;
+        System.out.println("Boolean: " + myBool);
+
+        // String data type (not a primitive type, but commonly used)
+        String myString = "Hello, World!";
+        System.out.println("String: " + myString);
+    }
+}
