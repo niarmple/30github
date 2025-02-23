829e670af6d86a321e3c36f03b619a4e82d006a9
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-24 00:37:34 +0600

    Create PalindromeNumber.java

---
+++ PalindromeNumber.java
@@ -0,0 +1,21 @@
+public class PalindromeNumber {
+    public static void main(String[] args) {
+        int number = 12321; // Example number
+        if (isPalindrome(number)) {
+            System.out.println(number + " is a palindrome.");
+        } else {
+            System.out.println(number + " is not a palindrome.");
+        }
+    }
+
+    public static boolean isPalindrome(int number) {
+        int originalNumber = number;
+        int reversedNumber = 0;
+        while (number != 0) {
+            int digit = number % 10;
+            reversedNumber = reversedNumber * 10 + digit;
+            number /= 10;
+        }
+        return originalNumber == reversedNumber;
+    }
+}
