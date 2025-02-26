9b995e50f547cb6be9501a85ec3ef14912d1a3fa
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-27 00:14:40 +0600

    Create SingleDigitCheck.java

---
+++ SingleDigitCheck.java
@@ -0,0 +1,15 @@
+public class SingleDigitCheck {
+    public static void main(String[] args) {
+        int number = 7; // You can change this number to test other values
+
+        if (isSingleDigit(number)) {
+            System.out.println(number + " is a single-digit number.");
+        } else {
+            System.out.println(number + " is not a single-digit number.");
+        }
+    }
+
+    public static boolean isSingleDigit(int number) {
+        return number >= -9 && number <= 9;
+    }
+}
