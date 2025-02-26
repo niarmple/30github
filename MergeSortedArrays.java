4a3278a49cb12fbb3dfbf6b7256a399ef1024c13
niarmple <91364842+niarmple@users.noreply.github.com>
2025-02-26 06:32:30 +0600

    Create MergeSortedArrays.java

---
+++ MergeSortedArrays.java
@@ -0,0 +1,42 @@
+public class MergeSortedArrays {
+    public static int[] merge(int[] arr1, int[] arr2) {
+        int n1 = arr1.length;
+        int n2 = arr2.length;
+        int[] mergedArray = new int[n1 + n2];
+        
+        int i = 0, j = 0, k = 0;
+        
+        // Traverse both arrays and merge them
+        while (i < n1 && j < n2) {
+            if (arr1[i] <= arr2[j]) {
+                mergedArray[k++] = arr1[i++];
+            } else {
+                mergedArray[k++] = arr2[j++];
+            }
+        }
+        
+        // Copy remaining elements of arr1, if any
+        while (i < n1) {
+            mergedArray[k++] = arr1[i++];
+        }
+        
+        // Copy remaining elements of arr2, if any
+        while (j < n2) {
+            mergedArray[k++] = arr2[j++];
+        }
+        
+        return mergedArray;
+    }
+
+    public static void main(String[] args) {
+        int[] arr1 = {1, 3, 5, 7};
+        int[] arr2 = {2, 4, 6, 8};
+        
+        int[] mergedArray = merge(arr1, arr2);
+        
+        System.out.println("Merged Array: ");
+        for (int num : mergedArray) {
+            System.out.print(num + " ");
+        }
+    }
+}
