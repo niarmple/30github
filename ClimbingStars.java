61214c262ccd2523f96b85d9b557a498a5b57dbc
niarmple <91364842+niarmple@users.noreply.github.com>
2025-03-03 01:43:14 +0600

    Update ClimbingStars.java

--- ClimbingStars.java
+++ ClimbingStars.java
@@ -1,6 +1,6 @@
 public class ClimbingStars {
     public static void main(String[] args) {
-        int rows = 5; // Number of rows for the climbing stars pattern
+        int rows = 5; // Numberrr of rows for the climbing stars pattern
 
         for (int i = 1; i <= rows; i++) {
             for (int j = 1; j <= i; j++) {
