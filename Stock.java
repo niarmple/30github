456de1917cb1c793313463f28888093758ed3aa0
niarmple <91364842+niarmple@users.noreply.github.com>
2025-03-04 19:29:40 +0600

    Create Stock.java

---
+++ Stock.java
@@ -0,0 +1,28 @@
+// Java Program to solve stock buy and sell
+// using one traversal
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Stock {
+
+    static int maxProfit(int[] prices) {
+        int minSoFar = prices[0];
+        int res = 0;
+
+        // Update the minimum value seen so far
+          // if we see smaller
+        for (int i = 1; i < prices.length; i++) {
+            minSoFar = Math.min(minSoFar, prices[i]);
+           
+            // Update result if we get more profit                
+            res = Math.max(res, prices[i] - minSoFar);
+        }
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] prices = {7, 10, 1, 3, 6, 9, 2};
+        System.out.println(maxProfit(prices));
+    }
+}
