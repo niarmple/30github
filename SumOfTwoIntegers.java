d8eb865e0e6804c5f719b590265488f2b3582e86
niarmple <91364842+niarmple@users.noreply.github.com>
2025-03-07 00:53:24 +0600

    Create SumOfTwoIntegers.java

---
+++ SumOfTwoIntegers.java
@@ -0,0 +1,13 @@
+public class SumOfTwoIntegers {
+    public static void main(String[] args) {
+        // Define two integers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Calculate the sum
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
