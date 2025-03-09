c34583cfc44556e57f902fac60e8e40e335f5767
niarmple <91364842+niarmple@users.noreply.github.com>
2025-03-10 02:12:33 +0600

    Update Arrays.java

--- Arrays.java
+++ Arrays.java
@@ -1,20 +1,12 @@
-// Java Program to find the largest element in 
-// array using iterative approach
-
 class Arrays {
   
-    // Array declared
+    
     static int arr[] = {20, 10, 20, 4, 100}; 
     
-    // Method to find maximum in arr[] 
     static int largest() {
       
         int i; 
-        
-        // Initialize maximum element 
         int max = arr[0]; 
-        
-          // Traversing and comparing max element
         for (i = 1; i < arr.length; i++) 
             if (arr[i] > max) 
                 max = arr[i]; 
