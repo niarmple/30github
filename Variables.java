51196361470bde1b0154a0594b550d5711839969
niarmple <91364842+niarmple@users.noreply.github.com>
2025-03-10 02:17:08 +0600

    Update Variables.java

--- Variables.java
+++ Variables.java
@@ -1,42 +1,24 @@
 import java.io.*;
 
 public class Variables {
-    // Java Program to show the use of 
-// Local Variablesssss
-
+   
     public static void main(String[] args)
     {
-        // x is a local variable
         int x = 10;
-
-        // message is also a local
-        // variable
         String message = "Hello, world!";
 
         System.out.println("x = " + x);
         System.out.println("message = " + message);
 
         if (x > 5) {
-            // result is a
-            // local variable
             String result = "x is greater than 5";
             System.out.println(result);
         }
 
-        // Uncommenting the line below will result in a
-        // compile-time error System.out.println(result);
-
         for (int i = 0; i < 3; i++) {
             String loopMessage
                 = "Iteration "
-                  + i; // loopMessage is a local variable
+                  + i;
             System.out.println(loopMessage);
         }
-
-        // Uncommenting the line below will result in a
-        // compile-time error
-        // System.out.println(loopMessage);
-    }
-}
-
 
